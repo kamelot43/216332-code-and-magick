fd0a4e6c621413823280c9967da9ec54924dfac3
kamelot43 <andrepud@mail.ru>
2017-09-04 15:26:56 +0300

    перетаскивание элементов

--- js/setup.js
+++ js/setup.js
@@ -181,4 +181,45 @@
   wizardFireball.addEventListener('click', function () {
     wizardFireball.style.background = getRandomElem(WIZARD_FIREBALL);
   });
+
+
+  // работа с перемещение элементов
+
+  var shopElement = document.querySelector('.setup-artifacts-shop');
+  var draggedItem = null;
+
+  shopElement.addEventListener('dragstart', function (evt) {
+    if (evt.target.tagName.toLowerCase() === 'img') {
+      draggedItem = evt.target;
+      evt.dataTransfer.setData('text/plain', evt.target.alt);
+      artifactsElement.style.outline = '2px dotted red';
+    }
+  });
+
+  var artifactsElement = document.querySelector('.setup-artifacts');
+
+  artifactsElement.addEventListener('dragover', function (evt) {
+    evt.preventDefault();
+  });
+
+  artifactsElement.addEventListener('drop', function (evt) {
+    evt.target.style.backgroundColor = '';
+    artifactsElement.style.outline = '';
+    evt.target.appendChild(draggedItem);
+    evt.preventDefault();
+    document.removeEventListener('dragenter', test);
+  });
+
+
+  artifactsElement.addEventListener('dragenter', function (evt) {
+    evt.target.style.backgroundColor = 'yellow';
+    evt.preventDefault();
+  });
+
+  artifactsElement.addEventListener('dragleave', function (evt) {
+    evt.target.style.backgroundColor = '';
+    evt.preventDefault();
+  });
+
+
 })();
