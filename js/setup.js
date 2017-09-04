cc2420d23bfb9d302a2c109077dab40b599b1aae
kamelot43 <andrepud@mail.ru>
2017-09-04 15:50:39 +0300

    оптимизация

--- js/setup.js
+++ js/setup.js
@@ -1,81 +1,9 @@
 'use strict';
 (function () {
 
-  window.setup = document.querySelector('.setup');
-  var similarListElement = setup.querySelector('.setup-similar-list');
 
   setup.querySelector('.setup-similar').classList.remove('hidden');
 
-  var WIZARD_NAMES = [
-    'Иван',
-    'Хуан Себастьян',
-    'Мария',
-    'Кристоф',
-    'Виктор',
-    'Юлия',
-    'Люпита',
-    'Вашингтон'
-  ];
-  var WIZARD_SURNAMES = [
-    'да Марья',
-    'Верон',
-    'Мирабелла',
-    'Вальц',
-    'Онопко',
-    'Топольницкая',
-    'Нионго',
-    'Ирвинг'
-  ];
-  var WIZARD_COUTS = [
-    'rgb(101, 137, 164)',
-    'rgb(241, 43, 107)',
-    'rgb(146, 100, 161)',
-    'rgb(56, 159, 117)',
-    'rgb(215, 210, 55)',
-    'rgb(0, 0, 0)'
-  ];
-
-  var WIZARD_FIREBALL = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
-  var WIZARD_EYES = ['black', 'red', 'blue', 'yellow', 'green'];
-  var WIZARD_GROUP = 4;
-
-  function getRandomElem(array) {
-    var rand = Math.floor(Math.random() * array.length);
-    return array[rand];
-  }
-
-  var WizardTemplate = document.querySelector('#similar-wizard-template').content;
-  var fragment = document.createDocumentFragment();
-
-  function createWizards(param) {
-    var wizards = [];
-    for (var i = 0; i < param; i++) {
-      wizards[i] = {
-        name: getRandomElem(WIZARD_NAMES) + ' ' + getRandomElem(WIZARD_SURNAMES),
-        coatColor: getRandomElem(WIZARD_COUTS),
-        eyesColor: getRandomElem(WIZARD_EYES)
-      };
-    }
-    return wizards;
-  }
-
-  var wizards = createWizards(WIZARD_GROUP);
-
-  function renderWizard(wizard) {
-    var wizardElement = WizardTemplate.cloneNode(true);
-
-    wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
-    wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
-    wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
-
-    return wizardElement;
-  }
-
-  for (var i = 0; i < wizards.length; i++) {
-    fragment.appendChild(renderWizard(wizards[i]));
-  }
-
-  similarListElement.appendChild(fragment);
 
   // Работа с обработчиками событий
   var setupOpen = document.querySelector('.setup-open');
@@ -152,34 +80,6 @@
         setupUserName.setCustomValidity('');
       }
     }
-  });
-
-  // Изменение цвета заливки
-  function getRandomFill(elem, elemValue, array) {
-    var result = getRandomElem(array);
-    elemValue.value = result;
-    elem.style.fill = result;
-  }
-
-  var wizardCout = setup.querySelector('.wizard-coat');
-  var coatColor = document.getElementsByName('coat-color');
-  var wizardEyes = setup.querySelector('.wizard-eyes');
-  var eyesColor = document.getElementsByName('eyes-color');
-  var wizardFireball = setup.querySelector('.setup-fireball-wrap');
-
-  // Изменение цвета мантии мага
-  wizardCout.addEventListener('click', function () {
-    getRandomFill(wizardCout, coatColor[0], WIZARD_COUTS);
-  });
-
-  // Изменени цвета глаз мага
-  wizardEyes.addEventListener('click', function () {
-    getRandomFill(wizardEyes, eyesColor[0], WIZARD_EYES);
-  });
-
-  // Изменени цвета глаз мага
-  wizardFireball.addEventListener('click', function () {
-    wizardFireball.style.background = getRandomElem(WIZARD_FIREBALL);
   });
 
 
